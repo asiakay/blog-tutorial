4a9875d75561887d6c26bfb8f393508d58a6543d
Asia K <asialakaygrady@gmail.com>
2022-12-11 01:05:41 -0500

    revision

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,5 @@
 import { json } from "@remix-run/node";
-import { 
-  Link, 
-  useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
 import { getPosts } from "~/models/post.server";
 
